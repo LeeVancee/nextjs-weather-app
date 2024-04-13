b092b5b40fa222858e0088226e2d956b5c699cf3
LeeVancee <1280403599@qq.com>
2024-04-14 02:56:05 +0800

    test

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { CommandDialogDemo } from "@/components/CommandDialogDemo"
+import { useEffect } from "react"
+
+export default function Page() {
+  ;<CommandDialogDemo />
+}
